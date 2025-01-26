beeff2f722cf44c48965f6ad75b3c4e084f20851
aborruso <aborruso@gmail.com>
2025-01-26 12:20:30 +0100

    update

--- script/progetti_finanziati.sh
+++ script/progetti_finanziati.sh
@@ -31,21 +31,23 @@ for ((page=1; page<=pages; page++))
 do
     # Formatta il numero di pagina con zero padding
     page_num=$(printf "%02d" $page)
-    
+
     echo "Scaricando pagina $page di $pages..."
-    
+
     curl 'https://api.tech.ec.europa.eu/search-api/prod/rest/search?apiKey=SEDIA_NONH2020_PROD&text=***&pageSize=50&pageNumber='"$page" -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: it,en-US;q=0.9,en;q=0.8' -H 'Cache-Control: No-Cache' -H 'Connection: keep-alive' -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryuBnndOmJO94KT19m' -H 'Origin: https://ec.europa.eu' -H 'Referer: https://ec.europa.eu/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-site' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' -H 'X-Requested-With: XMLHttpRequest' -H 'sec-ch-ua: "Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' --data-raw $'------WebKitFormBoundaryuBnndOmJO94KT19m\r\nContent-Disposition: form-data; name="sort"; filename="blob"\r\nContent-Type: application/json\r\n\r\n{"order":"DESC","field":"title"}\r\n------WebKitFormBoundaryuBnndOmJO94KT19m\r\nContent-Disposition: form-data; name="query"; filename="blob"\r\nContent-Type: application/json\r\n\r\n{"bool":{"must":[{"terms":{"programId":["44181033"]}}]}}\r\n------WebKitFormBoundaryuBnndOmJO94KT19m\r\nContent-Disposition: form-data; name="languages"; filename="blob"\r\nContent-Type: application/json\r\n\r\n["en"]\r\n------WebKitFormBoundaryuBnndOmJO94KT19m--\r\n' > "${folder}"/../data/progetti_finanziati/raw/progetti_finanziati_"$page_num".json
-    
+
     # Aggiungi un piccolo ritardo per evitare di sovraccaricare il server
     sleep 1
 done
 
 echo "Download completato. File salvati in data/progetti_finanziati/raw/"
 
+# Crea un file di esempio con i primi 3 progetti con tutti i campi
+jq '.results[0:3]' "${folder}"/../data/progetti_finanziati/raw/progetti_finanziati_01.json > "${folder}"/../data/sample/progetti_finanziati_sample_raw.json
+
 exit 0
 
-# Crea un file di esempio con i primi 3 progetti con tutti i campi
-jq '.results[0:3] | map(.metadata.participants |= (if type == "array" then map(fromjson)[0] else . end))' "${folder}"/../data/progetti_finanziati/raw/progetti_finanziati.json > "${folder}"/../data/sample/progetti_finanziati_sample.json
+jq '.results[0:3] | map(.metadata.participants |= (if type == "array" then map(fromjson)[0] else . end))' "${folder}"/../data/progetti_finanziati/raw/progetti_finanziati_01.json > "${folder}"/../data/sample/progetti_finanziati_sample.json
 
 # versione clean
 jq '.results | map(.metadata.participants |= (if type == "array" then map(fromjson)[0] else . end))' "${folder}"/../data/progetti_finanziati/raw/progetti_finanziati.json > "${folder}"/../data/progetti_finanziati/processing/progetti_finanziati.json
